74b0ca4a2f6123128505f481eb89aeb361f1a7c0
Kreshnik Mati <kmati@kmati.local>
2017-07-23 07:11:13 -0400

    changes

---
+++ test/doc-json-parse.js
@@ -0,0 +1,86 @@
+//var request = require('supertest');
+//var async = require('async');
+var nsf = require('../src/internal/tst.js');
+var compose = require('lodash.compose');
+var parser = require('../grammars/parser-json.js');
+
+// var host = process.argv[2];
+
+// if( !host ){
+
+// 	process.stderr.write("Host parameter required");
+// 	process.exit(9);
+
+// }
+
+// request = request('http://' + host + ":8080");
+
+// var ALIEN_DOC_ID = "doc1234";
+// var ALIEN_PURS_ID = "purs1234";
+// var ALIEN_DOC_HTML_ID = "doc-html-1234"
+// var ALIEN_HTML_ID = "html1234";
+
+var doc_src = compose(
+    nsf.makeNode("434", { t: "main = log \"Hello, World!\"\n", u: {type:"text"}})
+    , nsf.makeNode("433", { t: "import Control.Monad.Eff.Console\n", u: {type:"text"}})
+    , nsf.makeNode("422", { t: "module Main where\n", u: {dtype:"text"}})
+)();
+
+var purs_src = compose(
+    nsf.makeNode( ALIEN_DOC_ID, { t: "main.purs", u: {type:"doc", alien: "divsense/doc"}})
+)();
+
+var doc_html_src = compose(
+    nsf.setChildNodes("111", ["112", "125"])
+    , nsf.setChildNodes("112", ["113", "115"])
+    , nsf.setChildNodes("113", ["114"])
+    , nsf.setChildNodes("115", ["215"])
+    , nsf.setChildNodes("215", ["116"])
+    , nsf.setChildNodes("116", ["117", "118"])
+    , nsf.setChildNodes("118", ["119", "121", "122", "123", "124"])
+    , nsf.setChildNodes("119", ["120"])
+    , nsf.setChildNodes("125", ["126", "128", "130", "132", "134", "136"])
+    , nsf.setChildNodes("126", ["127"])
+    , nsf.setChildNodes("128", ["129"])
+    , nsf.setChildNodes("130", ["131"])
+    , nsf.setChildNodes("132", ["133"])
+    , nsf.setChildNodes("134", ["135"])
+    , nsf.setChildNodes("136", ["137"])
+
+
+    , nsf.makeNode("111", { t: "[", u: {type:"text"}})
+    , nsf.makeNode("112", { t: "{", u: {type:"text"}})
+    , nsf.makeNode("113", { t: "a", u: {type:"text"}})
+    , nsf.makeNode("114", { t: "html", u: {type:"text"}})
+    , nsf.makeNode("115", { t: "b", u: {type:"text"}})
+    , nsf.makeNode("215", { t: "[", u: {type:"text"}})
+    , nsf.makeNode("116", { t: "[", u: {type:"text"}})
+    , nsf.makeNode("117", { t: "children", u: {type:"text"}})
+    , nsf.makeNode("118", { t: "[", u: {type:"text"}})
+    , nsf.makeNode("119", { t: "\"", u: {type:"text"}})
+    , nsf.makeNode("120", { t: "10", u: {type:"text"}})
+    , nsf.makeNode("121", { t: "11", u: {type:"text"}})
+    , nsf.makeNode("122", { t: "true", u: {type:"text"}})
+    , nsf.makeNode("123", { t: "false", u: {type:"text"}})
+    , nsf.makeNode("124", { t: "null", u: {type:"text"}})
+    , nsf.makeNode("125", { t: "{", u: {type:"text"}})
+    , nsf.makeNode("126", { t: "nr", u: {type:"text"}})
+    , nsf.makeNode("127", { t: "12", u: {type:"text"}})
+    , nsf.makeNode("128", { t: "bool", u: {type:"text"}})
+    , nsf.makeNode("129", { t: "true", u: {type:"text"}})
+    , nsf.makeNode("130", { t: "f", u: {type:"text"}})
+    , nsf.makeNode("131", { t: "false", u: {type:"text"}})
+    , nsf.makeNode("132", { t: "obj", u: {type:"text"}})
+    , nsf.makeNode("133", { t: "{", u: {type:"text"}})
+    , nsf.makeNode("134", { t: "a", u: {type:"text"}})
+    , nsf.makeNode("135", { t: "[", u: {type:"text"}})
+    , nsf.makeNode("136", { t: "\"", u: {type:"text"}})
+    , nsf.makeNode("137", { t: "11", u: {type:"text"}})
+
+)();
+
+var doc_html_src_ord = doc_html_src.reverse();
+//console.log(JSON.stringify(doc_html_src_ord, null, 2))
+var json = parser.parse(nsf, doc_html_src_ord);
+
+console.log(JSON.stringify(json, null, 4))
---
+++ test/parser-json.js
@@ -0,0 +1,130 @@
+/**
+   parser functions
+   Convert a Divsense map to a json file
+*/
+
+exports.parse = function(nsf, src){
+    let mMap = nsf.mmapToMap(src);
+    
+    // for now use text to decide node type. could use an actual type property later
+    // nodes for xml are either attributes, text or element
+    let node_type = (current_state, node)  => {
+        if(    current_state === '[-"'   || current_state === '{-key-"'  || current_state === '{' ||
+               current_state === 'term'  || current_state === 'string'   ||
+               current_state === 'value' || current_state === 'ignore')
+            return '_';
+        
+        switch(node.t){
+        case '[' :
+        case '{' :
+        case '"' :
+        case 'true' :
+        case 'false' :
+        case 'null' :
+            return node.t; break;
+        default:
+            return '_'; break;
+        }
+    }
+
+    // underscore '_'  stands for anything, dash '-'  is reserved as the separator
+    let states = [
+        ['[',         '[',      '['],      // array followed array means another array started
+        ['[',         '{',      '{'],      // array followed by object means an object just started
+        ['[',         '"',      '"'],      // array followed by double quote means, surely a string item for the array is coming next
+        ['"',         '_',      'string'], // double quote followed by '_', value is a definite string (doesn't matter of parent is array or object
+        ['[',         'true',   'term'],   // array followed by true, a boolean true item is next
+        ['[',         'false',  'term'],   // array followed by false, a boolean false item is next
+        ['[',         'null',   'term'],   // array followed by null, a boolean false item is next
+        ['[',         '_',      'value'],  // array followed by anything, an array item that needs to be analyzed is coming next
+        ['{',         '_',      '{-key'],  // object followed by '_', object key
+        ['{-key',     '"',      '"'],      // object key followed by '"', object value is string and starts next
+        ['{-key',     '[',      '['],      // object key followed by '[', object value is an array
+        ['{-key',     '{',      '{'],      // object key followed by '{', object value is an object
+        ['{-key',     'true',   'term'],   // object followed by 'true', boolean true is value
+        ['{-key',     'false',  'term'],   // object followed by 'false', boolean false is value
+        ['{-key',     'null',   'term'],   // object followed by 'null', null is value
+        ['{-key',     '_',      'value'],  // object followed by '_', value needs to be analyzed (could be number), otherwise string
+        ['term',      '_',      'ignore'], // term (terminal node) followed by anything should be ignored
+        ['string',    '_',      'ignore'], // string followed by anything should be ignored for now as well
+        ['value',     '_',      'ignore'], // value followed by anything should be ignored for now as well
+        ['ignore',    '_',      'ignore'], // value followed by anything should be ignored for now as well
+    ];
+
+    let computations = {
+        '['(node, parent, prep, sib, ch){
+            // children items are already an array. Just push to siblings items
+            sib.items.push(ch.items)
+            return sib
+        },            
+        '{'(node, parent, prep, sib, ch){
+            // children items are to be transformed into an object
+            // from an array of key value tuples to object key val: [{key,val}...] -> {key:val,...}
+            let obj = ch.items.reduce( (acc, item) => {
+                acc[item.key] = item.val;
+                return acc;
+            }, {});
+            
+            sib.items.push(obj)
+            return sib
+        },        
+        '{-key'(node, parent, prep, sib, ch){
+            sib.items.push({key:node.t, val:ch.items[0]})
+            return sib
+        },      
+        '"'(node, parent, prep, sib, ch){
+            sib.items.push(ch.items[0])
+            return sib
+        },      
+        'string'(node, parent, prep, sib, ch){
+            sib.items.push(node.t)
+            return sib
+        },      
+        'term'(node, parent, prep, sib, ch){
+            switch(node.t){
+               case "true": sib.items.push(true); break;
+               case "false": sib.items.push(false); break;
+               case "null": sib.items.push(null); break;
+            }
+            return sib
+        },      
+        'ignore'(node, parent, prep, sib, ch){
+            return sib
+        },
+        'value'(node, parent, prep, sib, ch){
+            // value can be a number or a string
+            let val = (isNaN(node.t)) ? node.t : Number(node.t);
+            sib.items.push(val)
+            return sib
+        }
+    }
+
+    let state_machine = nsf.stateMachine(states, node_type);
+
+    // this gets called at each traversal step
+    let prep_call = (node, fromParent) => {
+        //let {_id, p, t, a:[ [, cm] = [] ] = [] } = node;
+
+        let {state = node.t} = fromParent;
+        let nextState = state_machine.next(state, node);
+        return {state:nextState, node:node}
+    };
+
+    // this gets called after the children of this node finally have a computed value which was passed to this function
+    let compute_call = (node, fromParent, fromPrep, siblings_computed, children_computed) => {
+        // only items array is really needed and each parent will decide how to reduce it
+        let siblings = nsf.ensureAll(siblings_computed, ['items'], []);
+        let children = nsf.ensureAll(children_computed, ['items'], []);
+
+        return computations[fromPrep.state](node, fromParent, fromPrep, siblings, children)
+    };
+      
+    // The callback's return value will be used to be passed along to the node's children, so information that the
+    // children need about this node, should be returned from the callback function
+    var out = nsf.traversePure(src, prep_call, compute_call);
+
+    return out.items[0];
+
+}
+
+
