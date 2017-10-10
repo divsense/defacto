405b3c47f9c26c12e16607580d5323a3aaaa898a
Hak Nick <haknick@gmail.com>
2017-10-10 04:48:25 -0400

    update

--- src/parse.js
+++ src/parse.js
@@ -2,7 +2,7 @@ import _kot from 'internal/_kot';
 
 
 /**
- * (Example: ramda/src/internal/_clone)
+ * (Example: examples/parsing.js)
  * 
  * This function is curried. In other words you can pass only parsing
  *  and then apply it to any other divsenseTree you desire.
@@ -13,15 +13,15 @@ import _kot from 'internal/_kot';
  * @memberOf defacto
  * @since v0.1.0
  * @category Function
- * @sig parsing -> divsenseTree -> *
- * @param {JSON} parsing Any number of positional arguments.
- * @param {JSON} divsenseTree The divsense tree to be parsed.
- * @return {*} The same tree in a completely different type format,
- *         which is based on 'parsing'.
+ * @sig a -> b -> *
+ * @param {Object} parsing The parsing language definition
+ * @param {Object} divsenseTree The divsense tree to be parsed.
+ * @return {String} The same tree in a completely different type format,
+ *         as String, which is based on 'parsing'.
  * @see choices
  * @example
- *
- *      parse(
+ *   import parse from parse.js
+ *   parse(a,b)
  */
 export default function(parsing, divsenseTree){
   return _kot('parsing');
